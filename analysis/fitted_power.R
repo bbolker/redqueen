72b7af7f1f396c62e7ea75654ab2d6a873cbf00c
parksw3 <parksw3@mcmaster.ca>
2017-08-29 20:50:26 -0400

    more confusing results

--- analysis/fitted_power.R
+++ analysis/fitted_power.R
@@ -2,11 +2,11 @@ source("../R/powerfun.R")
 load("../data/fitted_sim.rda")
 
 sites <- seq(10, 30, by=5)
-samples <- seq(25, 200, by=25)
+samples <- seq(25, 150, by=25)
 nsim <- 10
 
 simlist <- list(
-##    dagan=dagan_sim,
+    dagan=dagan_sim,
     vergara=vergara_sim
 )
 
@@ -16,7 +16,7 @@ test_list <- list(
     spearman=test_spearman
 )
 
-reslist <- vector('list', length(simlist))
+reslist <- list()
 
 for(sim_name in names(simlist)) {
     sim <- simlist[[sim_name]]
@@ -35,20 +35,24 @@ for(sim_name in names(simlist)) {
                         test=test_list[[test_name]]
                     )
                 ))
+                
                 sim_reslist[[j]] <- try(do.call('rbind', res))
-                if(!inherits(sim_reslist[[j]], "try-error")) sim_reslist[[j]]$sim <- j
+                if(!inherits(sim_reslist[[j]], "try-error")) {
+                    sim_reslist[[j]]$sim <- j
+                    sim_reslist[[j]]$sites <- rep(sites, each=nsim*length(sim[[j]]))
+                } 
                 cat(sim_name, test_name, i, j, "\n")
             }
                 
             sample_reslist[[i]] <- do.call('rbind', sim_reslist)
+            sample_reslist[[i]]$samples <- samples[i]
         }
         
         sample_res <- do.call('rbind', sample_reslist)
-        sample_res$samples <- rep(samples, each=nsim*length(sim)*length(sites))
         
         sub_reslist[[test_name]] <- sample_res
     }
-    simlist[[sim_name]] <- sub_reslist
+    reslist[[sim_name]] <- sub_reslist
 }
     
-save("simlist", file="fitted_power.rda")
+save("reslist", file="fitted_power.rda")
--- fig/fitted_power_fig.R
+++ fig/fitted_power_fig.R
@@ -1,17 +1,26 @@
 library(ggplot2); theme_set(theme_bw())
+library(tidyr)
 library(dplyr)
 load("../data/fitted_power.rda")
+load("../data/SMC_summary.rda")
 
 level <- 0.05
 
-simdf <- simlist %>%
+reslist2 <- reslist %>%
     lapply(bind_rows, .id="test") %>%
-    bind_rows(.id="sim") %>%
-    mutate(sites=as.factor(sites)) %>%
-    as.tbl
+    lapply(filter, !grepl("Error", effect.size)) ## remove try-error    
 
+simdf <- lapply(reslist2, function(x){
+    df <- as.data.frame(sapply(x[,-1], as.numeric))
+    df$test <- x[,1]
+    df
+}) %>%
+    bind_rows(.id="data") %>%
+    as.tbl %>%
+    mutate(sites=factor(sites))
+    
 sumdf <- simdf %>%
-    group_by(sim, test, sites, samples) %>%
+    group_by(data, test, sites, samples) %>%
     summarize(
         median=median(effect.size, na.rm=TRUE),
         lwr=quantile(effect.size, 0.025, na.rm=TRUE),
@@ -20,7 +29,7 @@ sumdf <- simdf %>%
 
 sig_sumdf <- simdf %>%
     filter(p.value < level) %>%
-    group_by(sim, test, sites, samples) %>%
+    group_by(data, test, sites, samples) %>%
     summarize(
         median=median(effect.size, na.rm=TRUE),
         lwr=quantile(effect.size, 0.025, na.rm=TRUE),
@@ -28,14 +37,14 @@ sig_sumdf <- simdf %>%
     )
 
 sig_simdf <- simdf %>%
-    group_by(sim, test, sites, samples) %>%
+    group_by(data, test, sites, samples) %>%
     summarize(power=mean(p.value<=level, na.rm=TRUE))
-    
+
 (gg_sum <- ggplot(sumdf, aes(samples, median, fill=sites)) +
     geom_ribbon(aes(ymax=upr, ymin=lwr), alpha=0.3) +
     geom_point(aes(col=sites)) +
     geom_line(aes(col=sites)) +
-    facet_grid(test~sim, scale="free") +
+    facet_grid(test~data, scale="free") +
     geom_hline(yintercept=0, lty=2))
 
 gg_sum %+% sig_sumdf
@@ -43,5 +52,30 @@ gg_sum %+% sig_sumdf
 ggplot(sig_simdf, aes(samples, power, col=sites)) +
     geom_point() +
     geom_line(lty=2) +
-    facet_grid(sim~test)
+    facet_grid(data~test)
 
+pardf <- clean_list$parlist %>%
+    filter(run==2) %>%
+    mutate(sim=1:50) %>%
+    rename(data=fit)
+
+alldf <- simdf %>% 
+    group_by(data,sim,sites,samples, test) %>%
+    summarize(
+        median.effect=median(effect.size, na.rm=TRUE),
+        power=mean(p.value<level, na.rm=TRUE)
+    ) %>%
+    merge(pardf) %>%
+    filter(samples==100, sites==20)
+
+ggplot(alldf, aes(value, median.effect, col=data)) +
+    geom_point() +
+    geom_smooth(method='lm') +
+    facet_grid(test~key, scale="free") +
+    geom_hline(yintercept=0, lty=2)
+
+ggplot(alldf, aes(value, power, col=data)) +
+    geom_point() +
+    geom_smooth(method='lm') +
+    scale_y_continuous(limits=c(0, 1)) +
+    facet_grid(test~key, scale="free_x")
