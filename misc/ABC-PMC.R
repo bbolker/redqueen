02a3fae35e94f92d68329d811299db4b6b7ac17c
parksw3 <parksw3@mcmaster.ca>
2018-05-05 22:19:44 -0400

    testing

---
+++ misc/ABC-PMC.R
@@ -0,0 +1,37 @@
+n <- 1000
+tmax <- 20
+
+samp <- rnorm(n)
+
+weight <- rep(1/n, n)
+
+theta.list <- vector('list', tmax)
+theta.list[[1]] <- samp
+
+weight.list <- vector('list', tmax)
+
+V <- 2 * var(samp)
+
+for (i in 2:tmax) {
+    samp <- sample(theta.list[[i-1]], prob=weight, replace=TRUE)
+    
+    theta <- rnorm(n, mean=samp, sd=sqrt(V))
+    
+    theta.list[[i]] <- theta
+    
+    weight <- dnorm(theta)/(
+        sapply(theta, function(x) sum(dnorm(theta.list[[i-1]], mean=x, sd=sqrt(V)) * weight))
+    )
+    
+    weight.list[[i]] <- weight <- weight/sum(weight)
+    
+    V <- 2 * cov.wt(as.data.frame(theta), wt=weight)$cov
+}
+
+plot(density(theta.list[[1]]))
+lines(density(theta.list[[2]], weight=weight.list[[2]]), col=2)
+lines(density(theta.list[[3]], weight=weight.list[[3]]), col=3)
+lines(density(theta.list[[20]], weight=weight.list[[20]]), col=4)
+
+plot(density(theta.list[[20]], weight=weight.list[[20]]))
+curve(dnorm(x), add=TRUE, col=2)
