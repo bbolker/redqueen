0c8bf84df9262176a42e8cbc39f697ca1357687f
unknown <parksw3@mcmaster.ca>
2018-08-21 20:55:36 -0400

    new figure

--- fig/simulated_data.R
+++ fig/simulated_data.R
@@ -5,7 +5,7 @@ library(gridExtra)
 library(readxl)
 library(ggplot2); theme_set(theme_bw(base_size = 12,
                                      base_family = "Times"))
-
+library(ellipse)
 source("../R/ABC_funs.R")
 source("../R/HPDregion.R")
 
@@ -44,7 +44,7 @@ for (n in load_names) {
     
     slist[[n]]$weight <- slist[[n]]$weight/sum(slist[[n]]$weight)
     
-    clist[[n]] <- HPDregion(slist[[n]], w=slist[[n]]$weight, prob=c(0.8, 0.95)) %>%
+    clist[[n]] <- HPDregion(slist[[n]], w=slist[[n]]$weight, prob=c(0.95)) %>%
         lapply(function(df) data.frame(
             level=df$level,
             x=df$x,
@@ -101,15 +101,60 @@ gg_df <- slist %>%
 gg_contour <- clist %>%
     bind_rows(.id="fit") %>%
     group_by(fit) %>%
-    mutate(prob=factor(level, labels=c("95%", "80%")))
+    mutate(prob=factor(level, labels=c("95%")))
 
-aggr <- list(mckone=mckone, vergara=vergara_obs, dagan=dagan) %>%
+aggr <- list(mckone=mckone, dagan=dagan) %>%
     bind_rows(.id="fit") %>%
-    mutate(fit=factor(fit, labels=data_name), sim=NA) 
+    mutate(fit=factor(fit, labels=data_name[-3]), sim=NA) 
+
+vg_ellipse <- vergara %>%
+    split(vergara$Site) %>%
+    lapply(function(x){
+        with(as.list(x), {
+            corr <- cov2cor(cov(data.frame(pinf, psex)))
+            ell <- ellipse(corr,
+                scale=c(sd(pinf), sd(psex)),
+                centre=c(mean(pinf), mean(psex)),
+                npoints=100)
+            as.data.frame(ell)
+        })
+    }) %>%
+    bind_rows(.id="Site")
+
+vsum <- vergara %>%
+    gather(key, value, -Site, -Year) %>%
+    group_by(key, Site) %>%
+    summarize(
+        mean=mean(value),
+        lwr=min(value),
+        upr=max(value)
+    ) %>%
+    mutate(fit=as.character(data_name[3]))
+
+vmean <- vsum %>%
+    dplyr::select(-upr, -lwr) %>%
+    spread(key, mean)
+
+vinfrange <- vsum %>%
+    filter(key=="pinf") %>%
+    mutate(
+        x=vsum$mean[1:4],
+        y=vsum$mean[5:8]
+    )
+
+vsexrange <- vsum %>%
+    filter(key=="psex") %>%
+    mutate(
+        x=vsum$mean[1:4],
+        y=vsum$mean[5:8]
+    )
 
 gsim <- ggplot(gg_contour) +
     geom_path(aes(x, y, linetype=prob, col=fit, group=interaction(level, region)), lwd=1) +
     geom_point(data=aggr,aes(pinf, psex), pch=2, size=2.5, col="black") +
+    geom_errorbarh(data=vinfrange, aes(x=x, y=y, xmin=lwr, xmax=upr), height=0.02, lty=2) +
+    geom_errorbar(data=vsexrange, aes(x=x, y=y, ymin=lwr, ymax=upr), width=0.02, lty=2) +
+    geom_point(data=vmean, aes(pinf, psex), pch=1, size=2.5, col="black") +
     scale_x_continuous("proportion infected", limits=c(0, 1), breaks=seq(0,1,0.2), expand=c(0, 0.07)) +
     scale_y_continuous("proportion sexual", limits=c(-0, 1), expand=c(0, 0.02)) +
     scale_colour_discrete(guide=FALSE) +
@@ -120,7 +165,7 @@ gsim <- ggplot(gg_contour) +
         strip.background = element_blank(),
         panel.border = element_rect(colour = "black"),
         panel.grid=element_blank(),
-        legend.position=c(0.08, 0.75)
+        legend.position="none"
     )
 
 if (save) ggsave("simulated_data.pdf", gsim, width=7, height=3)
